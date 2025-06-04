d5675822e3e20b025188602125dcb58995d377bb
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-06-05 00:57:48 +0530

    Clear main branch content

--- src/main/java/com/example/userr_service/Repo/UserrRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.userr_service.Repo;
-
-import com.example.userr_service.Model.Userr;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserrRepo extends JpaRepository<Userr, Long> {
-    public Userr findByUsername(String email);
-}
